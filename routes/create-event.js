0995879b2085e133dc7d4b52d609690e0a4a2587
Illya Velichko <bitbetmy@gmail.com>
2019-05-14 18:09:54 +0300

    Added delete event

--- routes/create-event.js
+++ routes/create-event.js
@@ -9,8 +9,8 @@ const _Events = require('../models/Events');
 // @    Method GET /events/all
 // @    Show all of the events
 // @    Public
-router.get('/all', (req, res) => {
-    _Events.find((err, events) => {
+router.get('/show/all', async (req, res) => {
+    await _Events.find((err, events) => {
         if(err) {
            return next(new Error(err));
         }
@@ -21,9 +21,9 @@ router.get('/all', (req, res) => {
 // @    Method GET /events/show:id
 // @    Show event by id
 // @    Public
-router.get('/show/:id', (req, res, next) => {
+router.get('/show/:id', async (req, res, next) => {
     const id = req.params.id;
-    _Events.findById(id, (err, events) => {
+    await _Events.findById(id, (err, events) => {
         if(err) {
             return next(new Error(err));
         }
@@ -60,10 +60,47 @@ router.post('/create', [
     }
 
 });
-// @    Method DELETE /events/delete
+
+// @    Method PUT /events/edit
 // @    Deleting event
 // @    Public
-router.delete('/delete', (req, res) => {
-
+router.put('/edit/:id', [
+    check('name').not().isEmpty(),
+    check('date').not().isEmpty()
+] ,async (req, res, next) => {
+    const id = req.params.id;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.status(401).json({errors: errors.array()});
+    }
+    await _Events.findById(id, async (error, events) => {
+        if(error) {
+            return next(new Error("'Wasn't found"));
+        }
+        events.name = req.body.name;
+        events.date = req.body.date;
+        events.save(
+            function (error, events) {
+                if(error)  { res.status(401).send('Unabled'); }
+                res.status(200).json(events);
+            }
+        )
+    })
 });
+
+// @    Method DELETE /events/delete/:id
+// @    Deleting event
+// @    Public
+router.delete('/delete/:id', async (req, res, next) => {
+    const id = req.params.id;
+    await _Events.findByIdAndDelete(id, function (err, event) {
+        if (err) {
+            return next(new Error('Todo was not found'));
+        }
+        res.json('Successfully removed')
+    })
+});
+
+
+
 module.exports = router;
